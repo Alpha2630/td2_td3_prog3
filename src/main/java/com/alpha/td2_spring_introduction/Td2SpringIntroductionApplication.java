47dee459f6be7de533a199332a6d5b7b073a6724
Alking <alrey2635@gmail.com>
2026-03-18 15:59:08 +0300

    first commit

---
+++ src/main/java/com/alpha/td2_spring_introduction/Td2SpringIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.td2_spring_introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td2SpringIntroductionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Td2SpringIntroductionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alpha/td2_spring_introduction/Td2SpringIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.td2_spring_introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td2SpringIntroductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
