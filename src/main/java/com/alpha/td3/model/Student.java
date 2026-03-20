24daf8bb93ed44c4d8237d4b308a29ace4f67ab3
Alking <alrey2635@gmail.com>
2026-03-20 11:08:03 +0300

    added model

--- src/main/java/com/alpha/td3/model/Student.java
+++ src/main/java/com/alpha/td3/model/Student.java
@@ -1,4 +1,16 @@
 package com.alpha.td3.model;
 
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
+    private String reference;
+    private String firstName;
+    private String lastName;
+    private int age;
 }
