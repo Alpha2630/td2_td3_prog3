05758b63cbe560316a0340ccf5c7585bd406db7f
Alking <alrey2635@gmail.com>
2026-03-25 20:22:47 +0300

    added the validator

--- src/main/java/com/alpha/td4/validator/StudentValidator.java
+++ src/main/java/com/alpha/td4/validator/StudentValidator.java
@@ -1,4 +1,27 @@
 package com.alpha.td4.validator;
 
+import com.alpha.td4.model.Student;
+import com.alpha.td4.exception.BadRequestException;
+
+import java.util.List;
+
 public class StudentValidator {
+
+    public void validate(List<Student> students) {
+
+        for (Student s : students) {
+
+            if (s.getReference() == null || s.getReference().isEmpty()) {
+                throw new BadRequestException("Reference manquante");
+            }
+
+            if (s.getFirstName() == null || s.getFirstName().isEmpty()) {
+                throw new BadRequestException("FirstName manquant");
+            }
+
+            if (s.getLastName() == null || s.getLastName().isEmpty()) {
+                throw new BadRequestException("LastName manquant");
+            }
+        }
+    }
 }
